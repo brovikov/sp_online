c4ded281350a60bd9e65c49ff9bd6c5eef05a776
Alexey S. Brovikov <alexey.brovikov@gmail.com>
2013-05-11 18:02:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpOnline::Application.config.secret_token = '10902f2505851cb83675faae65620a3e7b77d741606e29e515e4be2eca9ab7a08584ad3a856812ec1b485d884b1926c41027a73bd05f1c5d24c4a78a61b602cd'
